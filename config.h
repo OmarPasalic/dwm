e267ba7cc6e23f97ab96042d7d33e59b90d04c42
Omar <ocodingp@gmail.com>
2022-03-22 18:01:08 +0100

    Ultima versione

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 50;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "DejaVu Sans Mono:size=10" };
+static const char dmenufont[]       = "DejaVu Sans Mono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "\uf121", "\uf1eb", "\uf5da", "\uf001" };
+static const char *tags[] = { "\uf015", "\uf121", "\uf1eb", "\uf5da", "\uf001", "\uf11b" };
 
 static const Rule rules[] = {
 	/* xprop(1):
